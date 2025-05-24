878a41d674df58223298953a3821909bff687c78
user <spider@gmail.com>
2025-05-24 15:01:18 +0300

    Teacher entity add

--- FirstEFCoreIntro/Program.cs
+++ FirstEFCoreIntro/Program.cs
@@ -10,42 +10,73 @@ namespace FirstEFCoreIntro
             Console.InputEncoding = System.Text.Encoding.Unicode;
 
             AppDbContext db = new AppDbContext();
-            //#region Створення студента
-            //Student st = new Student();
-
-            //Console.WriteLine("Введіть ім'я студента: ");
-            //st.Name = Console.ReadLine();
-
-            //Console.WriteLine("Введіть вік студента: ");
-            //st.Age = int.Parse(Console.ReadLine());
-
-            //db.Students.Add(st);
-            //db.SaveChanges();
-            //#endregion
-            //#region Створення викладача
-            //Teacher th = new Teacher();
-
-            //Console.WriteLine("Введіть повне ім'я викладача: ");
-            //th.FullName = Console.ReadLine();
-
-            //Console.WriteLine("Введіть вік викладача: ");
-            //th.Age = int.Parse(Console.ReadLine());
-
-            //Console.WriteLine("Введіть заробітну плату викладача в UAN: ");
-            //th.Salary = int.Parse(Console.ReadLine());
-
-            //db.Teachers.Add(th);
-            //db.SaveChanges();
-            //#endregion
-            foreach (var s in db.Students)
+            int choose;
+            do
             {
-                Console.WriteLine(s.ToString());
-            }
+                Console.WriteLine("1. Створити студента");
+                Console.WriteLine("2. Створити викладача");
+                Console.WriteLine("3. Вивести студента");
+                Console.WriteLine("4. Вивести викладача");
+                Console.WriteLine("5. Вийти");
 
-            foreach (var t in db.Teachers)
-            {
-                Console.WriteLine(t.ToString());
-            }
+                choose = int.Parse(Console.ReadLine());
+
+                switch (choose)
+                {
+                    case 1:
+                        Student st = new Student();
+
+                        Console.WriteLine("Введіть ім'я студента: ");
+                        st.Name = Console.ReadLine();
+
+                        Console.WriteLine("Введіть вік студента: ");
+                        st.Age = int.Parse(Console.ReadLine());
+
+                        db.Students.Add(st);
+                        db.SaveChanges();
+                        break;
+
+                    case 2:
+                        Teacher th = new Teacher();
+
+                        Console.WriteLine("Введіть повне ім'я викладача: ");
+                        th.FullName = Console.ReadLine();
+
+                        Console.WriteLine("Введіть вік викладача: ");
+                        th.Age = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Введіть заробітну плату викладача в UAN: ");
+                        th.Salary = int.Parse(Console.ReadLine());
+
+                        db.Teachers.Add(th);
+                        db.SaveChanges();
+                        break;
+
+                    case 3:
+                        foreach (var s in db.Students)
+                        {
+                            Console.WriteLine(s.ToString());
+                        }
+                        break;
+
+                    case 4:
+                        foreach (var t in db.Teachers)
+                        {
+                            Console.WriteLine(t.ToString());
+                        }
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Вихід з програми.");
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } while (choose != 5);
+
         }
+    }
     }
 }
